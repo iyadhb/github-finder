b90305dfb08ee58561861165fd5a212b9b739c01
Iyad <iyadhb@gmail.com>
2020-09-16 05:36:06 +0100

    few changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
+import Navbar from './components/layouts/Navbar';
 import './App.css';
 
 class App extends Component {
   render() {
-    const name = 'Iyad Barakat';
-    const loading = false;
-    const showName = true;
     return (
       <div className='App'>
-        {loading ? <h4>...is loading</h4> : <h1> Hello {showName && name} </h1>}
+        <Navbar />
       </div>
     );
   }
